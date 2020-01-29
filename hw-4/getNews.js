a2fac4825c239fd9634e57835f5a011af8b96ef7
Elena Maltseva <lena@maltseva.design>
2020-01-29 21:51:06 +0300

    get news

--- hw-4/getNews.js
+++ hw-4/getNews.js
@@ -25,38 +25,40 @@ app.get('/', (req, res) => {
 
 app.post('/news', (req, res) => {
    let userChoice = Object.keys(req.body)
-   // getNews(userChoice)
-   // res.send('ok')
-   res.render('news', getNews(userChoice))
+
+   let promises = []
+   userChoice.forEach(el => {
+      promises.push(getNews(el))
+   })
+   Promise.all(promises).then((data) => {
+      res.send(data)
+      // res.render('news', data)
+   },
+   (err) => {
+      res.send(err)
+   })
 })
 
 app.listen(8000, () => {
    console.log('Server has been started')
 })
 
-
-
-
 function getNews (cat) {
-   let allNews = []
-   cat.forEach(el => {
-      let url = `https://yandex.ru/news/rubric/${el}?from=rubric`
+   let url = `https://yandex.ru/news/rubric/${cat}?from=rubric`
+   return new Promise( (resolve, reject) => {
       request(url, (err, response, body) => {
          if (!err && response.statusCode === 200) {
                const $ = cheerio.load(body)
                const news = $('.link.link_theme_black.i-bem')
-               let newsList = {cat: el, news: []}
+               const newsList = {category: cat, news: []};
                for (i = 0; i < news.length; i++) {
                   let newsItem = news.eq(i).text()
                   newsList.news.push(newsItem)
                }
-               // console.log(newsList)
-               allNews.push(newsList)
-               console.log(allNews)
-         } else console.log(err)
+               resolve(newsList);
+         } else reject(err);
       })
    })
-  
 }
 
 
