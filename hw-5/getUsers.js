880099f23099f5826f685e08fcd75a6dc2d91d9c
Elena Maltseva <lena@maltseva.design>
2020-02-05 02:06:07 +0300

    added CRUD functions

--- hw-5/getUsers.js
+++ hw-5/getUsers.js
@@ -4,28 +4,76 @@ const User = require('./models/user')
 
 const app = express()
 
+// Middleware
+app.use(express.json())
+
 mongoose.connect('mongodb://localhost:32772/insta', {
    useNewUrlParser: true, 
-   useUnifiedTopology: true
+   useUnifiedTopology: true,
+   useFindAndModify: false
+},
+(err) => {
+   if (!err) {
+      console.log('Database successfully connected')
+   } else throw err
 })
 
-// Routing
+app.listen(8000, () => {
+   console.log('Server listens http://localhost:8000')
+})
+
+// CRUD
+// CREATE
+app.post('/users', async (req, res) => {
+   const user = new User(req.body)
+   const savedUser = await user.save(err => {
+      if (!err) {
+         console.log('New user successfully added')
+      }
+   })
+   res.json(savedUser)
+})
+
+// READ
 app.get('/users', async (req, res) => {
    const users = await User.find()
    res.json(users)
 })
 
 app.get('/users/:id', async (req, res) => {
-   const user = await User.findById(req.params.id)
+   const user = await User.findById(req.params.id, (err, user) => {
+      if (err) {
+         res.send('The user is not found')
+      }
+      res.json(user)
+   })
+})
+
+// UPDATE
+app.put('/users/:id', async (req, res) => {
+   const user = await User.findByIdAndUpdate(req.params.id, req.body, (err) => {
+      if (err) {
+         res.send('The user is not found')
+      }
+   })
    res.json(user)
 })
 
-app.post('/users', async (req, res) => {
-   const user = new User(req.body)
-   const savedUser = await user.save()
-   res.json(savedUser)
+// DELETE
+app.delete('/users', async (req, res) => {
+   const user = await User.findOneAndDelete(req.body, (err, user) => {
+      if (err) {
+         res.send('The user is not found')
+      }
+      res.send(`The user successfully deleted.\n${user}`)
+   })
 })
 
-app.listen(8000, () => {
-   console.log('Server listens http://localhost:8000')
+app.delete('/users/:id', async (req, res) => {
+   const user = await User.findByIdAndDelete(req.params.id, (err, user) => {
+      if (err) {
+         res.send('The user is not found')
+      }
+      res.send(`The user successfully deleted.\n${user}`)
+   })
 })
--- hw-5/models/user.js
+++ hw-5/models/user.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-   firstName: {type: String, default: 'Vasya'},
-   lastName: {type: String, default: 'Pupkin'},
+   name: {
+      firstName: {type: String, default: 'Vasya'},
+      lastName: {type: String, default: 'Pupkin'},
+   },
    avatar: String,
    email: String,
-   bio: String
+   bio: String, 
 })
 
 module.exports = mongoose.model('User', userSchema, 'users')
